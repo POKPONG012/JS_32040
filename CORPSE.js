af9e3cfacad3d562bafd79aada54ee2d91183011
POKPONG012 <s6406021632040@email.kmutnb.ac.th>
2022-12-29 14:29:49 +0700

    initial commit

---
+++ CORPSE.js
@@ -0,0 +1,16 @@
+function myfuntion(){
+    var pizzaname = "Valvo";
+    //code here can use pizzaname
+}
+
+const logss = true;
+if (logss == true){
+    const statusMessage = 'user is login !!';     
+}
+
+
+
+
+
+console.log(statusMessage);
+
